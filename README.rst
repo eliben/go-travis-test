08ba3446671a0bc93086b80074d0d063edd7dc0b
Eli Bendersky <eliben@google.com>
2019-03-08 10:15:27 -0800

    Add changes

--- README.rst
+++ README.rst
@@ -3,3 +3,8 @@ Playing with Travis running go programs as part of the build
 .. image:: https://travis-ci.com/eliben/go-travis-test.png?branch=master
   :align: center
   :target: https://travis-ci.com/eliben/go-travis-test
+
+* TRAVIS_BRANCH
+* TRAVIS_PULL_REQUEST
+* TRAVIS_PULL_REQUEST_SHA
+
--- runtest.sh
+++ runtest.sh
@@ -6,11 +6,13 @@ git diff --name-only HEAD...$TRAVIS_BRANCH
 
 if [[ -z "$TRAVIS_PULL_REQUEST_SHA" ]]; then
   echo "TRAVIS_PULL_REQUEST_SHA not set"
+  echo "BAD BAD BAD BAD BAD"
 else
   echo "$TRAVIS_PULL_REQUEST_SHA"
   mergebase="$(git merge-base -- "$TRAVIS_BRANCH" "$TRAVIS_PULL_REQUEST_SHA")"
   echo $mergebase
   git diff --name-only "$mergebase" "$TRAVIS_PULL_REQUEST_SHA" --
+  git diff "$mergebase" "$TRAVIS_PULL_REQUEST_SHA" --
 fi
 
 echo "Running 'go run'"
